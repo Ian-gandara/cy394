0f9c61a32d9000783a01f4c94dcc5b7db49f4eb9
Ian-gandara <iangandara9@gmail.com>
2025-04-15 00:10:12 -0400

    Update db.py

--- hw1/flaskr/db.py
+++ hw1/flaskr/db.py
@@ -9,7 +9,7 @@ def get_db():
     again.
     """
     mydb = mysql.connector.connect(
-            host="db", user='flaskuser', password='flaskpass', database='flaskdb'
+            host="127.0.0.1", user='flaskuser', password='flaskpass', database='flaskdb'
         )
     print("Connected db:")
     
